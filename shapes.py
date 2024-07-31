dc1ff41dd175c6714d732e01c071ea37d211e9be
Farid Virani <fvirani786@gmail.com>
2024-07-30 20:58:05 -0500

    feat:first updates

---
+++ shapes.py
@@ -0,0 +1,17 @@
+
+
+class Shape:
+    def __init__(self, color):
+        self.color = color
+
+    def area(self):
+        return 0
+
+    def perimeter(self):
+        return 0
+
+    def __str__(self):
+        return f"Shape color: {self.color}"
+
+
+
