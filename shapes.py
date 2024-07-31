a2d2d597791615f73e2180cd205e2bdcee856e71
Farid Virani <fvirani786@gmail.com>
2024-07-30 21:17:24 -0500

    feat:more updates on 2nd class

--- shapes.py
+++ shapes.py
@@ -13,5 +13,15 @@ class Shape:
     def __str__(self):
         return f"Shape color: {self.color}"
 
+class Rectangle(Shape):
+    def __init__(self, color, width, height):
+        super().__init__(color)
+        self.width = width
+        self.height = height
 
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
 
